84dcc2838ac17327f603af282a1df9f3b5c9f54c
Luzarait1 <lcanasq@unal.edu.co>
2024-04-29 20:24:11 -0500

    Initial commit

---
+++ src/main/java/com/example/ProyectoIntegradoP/ProyectoIntegradoPApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoIntegradoP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntegradoPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIntegradoPApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProyectoIntegradoP/ProyectoIntegradoPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoIntegradoP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegradoPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
